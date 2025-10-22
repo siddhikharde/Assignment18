05bae1caef270fdf6423aa7e2d735b03f3d77373
siddhikharde <siddhikharde2005@gmail.com>
2025-10-22 16:45:21 +0530

    contact section created

--- Assignment-18/src/main.jsx
+++ Assignment-18/src/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import Navbar from './navbar'
 import Home from './home'
 import About from './about' 
+import Contact from './contact'
 
 createRoot(document.getElementById('root')).render(
   <>
  
    <Navbar />
+   <Contact/>
    <About />
    <Home/>
   </>
